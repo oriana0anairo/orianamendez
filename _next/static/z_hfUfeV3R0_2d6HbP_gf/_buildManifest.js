5ae49ab894cacc2f42fbf91dedf9de6b1b36b5cd
Oriana <orianamendezprato@gmail.com>
2026-03-19 18:17:50 -0500

    Deploy portfolio

---
+++ _next/static/z_hfUfeV3R0_2d6HbP_gf/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/orianamendez//_next/:path+",
+        "destination": "/orianamendez/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
